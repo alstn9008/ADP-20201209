5d91f452446f8db2023ab39e1f9ecba68a4e3974
Minsu Kim <alstn9008@naver.com>
2020-12-09 14:38:12 +0900

    Initial sharing of project

---
+++ MyApp.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class MyApp here.
+ *
+ * @author (2017210105 김민수, 2019315066 사사키야스오)
+ * @version (2020.12.09)
+ */
+public class MyApp
+{
+    
+}
---
+++ MyPanel.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class MyPanel here.
+ *
+ * @author (2017210105 김민수, 2019315066 사사키야스오)
+ * @version (2020.12.09)
+ */
+public class MyPanel
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class MyPanel
+     */
+    public MyPanel()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
